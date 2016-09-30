4c13dc0c7077a2e2b50975e359267d33a458057f
Pablo Elustondo <pablo.elustondo@soti.net>
2016-09-29 20:09:01 -0400

    we have a save button responding to events and writing a message

--- Matt/Matt/ViewController.swift
+++ Matt/Matt/ViewController.swift
@@ -13,13 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Message.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+// MARK: Properties
 
-
+    @IBOutlet weak var ConcernName: UITextField!
+ 
+    @IBOutlet weak var Message: UILabel!
+    
+// MARK: Action
+    
+    @IBAction func ConcernSave(sender: AnyObject) {
+        
+        Message.text = "saved succesfully"
+    }
 }
 
