3200639c6fa88f1358a1e303a67b3664836ad597
Pablo Elustondo <pablo.elustondo@soti.net>
2016-10-02 02:28:43 -0400

    starting to create the custom control

--- Matt/Matt/ConcernControl.swift
+++ Matt/Matt/ConcernControl.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 
-class ConcernControl: UIView {
+class ConcernControl: UIView{
 
     // MARK: Initialization
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
-        button.backgroundColor = UIColor.redColor()
+        let button = UILabel(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        button.backgroundColor = UIColor.yellowColor()
+        button.text = "1"
+        button.textAlignment = NSTextAlignment.Center
         addSubview(button)
+        let button2 = UILabel(frame: CGRect(x: 30, y: 0, width: 30, height: 30))
+        button2.backgroundColor = UIColor.greenColor()
+        addSubview(button2)
+        let button3 = UILabel(frame: CGRect(x: 60, y: 0, width: 30, height: 30))
+        button3.backgroundColor = UIColor.cyanColor()
+
+ 
+        addSubview(button3)
     }
-    
+
     override func intrinsicContentSize() -> CGSize {
         return CGSize(width: 200, height: 30)
     }
