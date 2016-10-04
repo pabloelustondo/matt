31942ef3a56d3a184324c2b333b562073bd8c361
Pablo Elustondo <pablo.elustondo@soti.net>
2016-10-03 22:53:38 -0400

    added some images

--- Matt/Matt/ConcernControl.swift
+++ Matt/Matt/ConcernControl.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ConcernControl: UIView{
 
+    // MARK: Properties
+    
+    var concernPoints:Int = 1
+    var subConcernConcerningPoints:Int = 2
+    var subConcentTotalPoints = 3
+    
     // MARK: Initialization
     
     required init?(coder aDecoder: NSCoder) {
@@ -18,11 +24,13 @@ class ConcernControl: UIView{
         let boxWidth = 30
         let middleBoxStart = (rightBorder - CGFloat(boxWidth))/2
      
+        //Images
+        let whiteStarImage = UIImage(named: "white-star")
         
         //Day achivement / comitment
         let button = UILabel(frame: CGRect(x: 0, y: 0, width: boxWidth, height: boxWidth))
         button.backgroundColor = UIColor.yellowColor()
-        button.text = "1"
+        button.text = String(concernPoints)
         button.textAlignment = NSTextAlignment.Center
         button.userInteractionEnabled = true;
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ConcernControl.dayButtonTapped(_:)))
@@ -32,16 +40,17 @@ class ConcernControl: UIView{
         
         
         //General Status
-        let button2 = UILabel(frame: CGRect(x: Int(middleBoxStart), y: 0, width: boxWidth, height: boxWidth))
+        let button2 = UIButton(frame: CGRect(x: Int(middleBoxStart), y: 0, width: boxWidth, height: boxWidth))
         button2.backgroundColor = UIColor.greenColor()
+        button2.setImage(whiteStarImage, forState: .Normal)
         addSubview(button2)
         
         
         //SubConcerns
         let button3 = UILabel(frame: CGRect(x: Int(rightBorder) - boxWidth, y: 0, width: boxWidth, height: boxWidth))
         button3.backgroundColor = UIColor.cyanColor()
-        button.text = "1/3"
-        button.textAlignment = NSTextAlignment.Center
+        button3.text = String(subConcernConcerningPoints) + "/" + String(subConcentTotalPoints)
+        button3.textAlignment = NSTextAlignment.Center
  
         addSubview(button3)
     }
