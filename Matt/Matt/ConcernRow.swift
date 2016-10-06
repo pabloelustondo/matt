e416c8019ffc7282cb2e353ebd8ecb7884d5e200
Pablo Elustondo <pablo.elustondo@soti.net>
2016-10-06 08:11:37 -0400

    starting to put the table view

---
+++ Matt/Matt/ConcernRow.swift
@@ -0,0 +1,14 @@
+//
+//  ConcernRow.swift
+//  Matt
+//
+//  Created by Pablo Elustondo on 2016-10-05.
+//  Copyright © 2016 Pablo Elustondo. All rights reserved.
+//
+
+import UIKit
+
+class ConcernRow: UIView {
+
+
+}
